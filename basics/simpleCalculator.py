39dca9576b909ea60b650ced12c0c607becae35f
devKiNGCRiC <kingcric28@gmail.com>
2026-04-06 22:44:48 +0530

    Simple Calculator : Addition , Subtraction, Multiplication and Division

---
+++ basics/simpleCalculator.py
@@ -0,0 +1,3 @@
+num1 = int(input("Enter First Number : "))
+num2 = int(input("Enter Second Number : "))
+print(f'Addition : {num1+num2}\nSubtraction : {num1-num2}\nMultiplication : {num1*num2}\nDivision : {round((num1/num2),3)}')
