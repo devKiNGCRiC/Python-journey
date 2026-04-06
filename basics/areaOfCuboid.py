97f7bd2cd5fc32060240c7c33bb9e2bc046c527c
devKiNGCRiC <kingcric28@gmail.com>
2026-04-06 22:53:44 +0530

    Surface area of Cuboid

---
+++ basics/areaOfCuboid.py
@@ -0,0 +1,5 @@
+length = int(input("Enter Length : "))
+width = int(input("Enter Width : "))
+height = int(input("Enter Height : "))
+
+print(f'Surface area of Cuboid : {2*((length*width)+(length*height)+(width*height))}')
